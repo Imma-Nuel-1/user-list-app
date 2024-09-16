cd3838a743c1e4951b6e3d15f3bad31f4a4cf4af
Imma~Nuel <Adesanyaoluwamuyiwa12@gmail.com>
2024-09-16 19:16:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import UserList from "./UserList";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Users List App</h1>
+      </header>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
